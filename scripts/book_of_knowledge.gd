38dc467a45bee03d42c9772e58b1b854d9d79617
lschiedel <lisa.schiedel02@gmail.com>
2025-03-29 16:36:37 +0100

    changed the access to the panel for bok

--- scripts/book_of_knowledge.gd
+++ scripts/book_of_knowledge.gd
@@ -10,11 +10,11 @@ func _ready():
 	bok_button.pressed.connect(_on_bok_pressed)
 	
 	 # Load the BookContent scene
-	book_content_instance = preload("res://scenes/book_of_knowledge.tscn").instantiate()
-	add_child(book_content_instance)  # Add the BookContent instance to the parent scene
+	#book_content_instance = preload("res://scenes/book_of_knowledge.tscn").instantiate()
+	#add_child(book_content_instance)  # Add the BookContent instance to the parent scene
 	
 	# Connect the back_button_pressed signal from BookContent to a function in the parent
-	var panel_node = book_content_instance.get_node("Panel(temp)")
+	var panel_node = bok.get_node("Panel(temp)")
 	panel_node.back_pressed.connect(_on_back_button_pressed)
 	
 func _on_bok_pressed():
