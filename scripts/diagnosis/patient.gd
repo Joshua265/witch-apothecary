9de052b7a9a2155bfcbff9725247b5b524e5014a
elenagaz <108979839+elenagaz@users.noreply.github.com>
2025-05-22 21:24:25 +0200

    added the characters - 2nd patient is not added

--- scripts/diagnosis/patient.gd
+++ scripts/diagnosis/patient.gd
@@ -4,7 +4,7 @@ extends TextureRect
 var image_path : String
 
 var patient_images = {
-	"helena": "res://sprites/characters/text.png",  
+	"helena": "res://sprites/characters/seamstress_sitting.png",  
 	"john": "res://john_image.png"
 }
 var current_patient : String = "helena" 
@@ -16,7 +16,9 @@ func _ready():
 func update_patient_image():
 	var loaded_texture = load(image_path)
 	if loaded_texture:
-		patient_image.texture = loaded_texture  
-		patient_image.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT  
+		patient_image.texture = loaded_texture
+		patient_image.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+		patient_image.expand = true
+		patient_image.custom_minimum_size = Vector2(128, 128)
 	else:
 		print("Failed to load texture: " + image_path)
